2dc735c09da77e9e8af51598435f2280920dc6a0
ARPIT CHOUDHARY <choudharyarpit99@gmail.com>
2020-06-07 17:48:11 +0530

    Create 518. Coin Change 2.java

---
+++ 518. Coin Change 2.java
@@ -0,0 +1,19 @@
+/*
+You are given coins of different denominations and a total amount of money.
+Write a function to compute the number of combinations that make up that amount.
+You may assume that you have infinite number of each kind of coin.
+*/
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[coins.length + 1][amount + 1];
+        dp[0][0] = 1;
+        for(int i = 1; i < dp.length; i++) {
+            dp[i][0] = 1;
+            for(int j = 1; j <= amount; j++) {
+                dp[i][j] = dp[i-1][j] + ((j  >= coins[i - 1])? dp[i][j - coins[i-1]] : 0);
+            }
+        }
+        return dp[coins.length][amount];
+    }
+}
