e040fc5f58918df320092191861deb3f69e71f81
ARPIT CHOUDHARY <choudharyarpit99@gmail.com>
2020-09-13 11:25:35 +0530

    Create 10.Regular Expression Matching.java

---
+++ 10.Regular Expression Matching.java
@@ -0,0 +1,19 @@
+/*
+Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+*/
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if (p.isEmpty()) 
+            return s.isEmpty();
+        boolean first_match = (!s.isEmpty()&&(p.charAt(0)==s.charAt(0)||p.charAt(0) == '.'));
+        if (p.length() >= 2 && p.charAt(1) == '*'){
+            return (isMatch(s,p.substring(2))||(first_match && isMatch(s.substring(1), p)));
+        } else {
+            return first_match && isMatch(s.substring(1), p.substring(1));
+        }
+    }
+}
