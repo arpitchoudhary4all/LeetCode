100b7215434569c6434ebf980c66c79677953d6d
ARPIT CHOUDHARY <choudharyarpit99@gmail.com>
2020-09-27 12:27:36 +0530

    Create 645.Set Mismatch.java

---
+++ 645.Set Mismatch.java
@@ -0,0 +1,27 @@
+/*
+The set S originally contains numbers from 1 to n. But unfortunately, due to the data error, 
+one of the numbers in the set got duplicated to another number in the set, 
+which results in repetition of one number and loss of another number.
+
+Given an array nums representing the data status of this set after the error.
+Your task is to firstly find the number occurs twice and then find the number that is missing. 
+Return them in the form of an array.
+*/
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        Map < Integer, Integer > map = new HashMap();
+        int dup = -1, missing = 1;
+        for (int n: nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        for (int i = 1; i <= nums.length; i++) {
+            if (map.containsKey(i)) {
+                if (map.get(i) == 2)
+                    dup = i;
+            } else
+                missing = i;
+        }
+        return new int[]{dup, missing};
+    }
+}
